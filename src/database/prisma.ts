0e78b43cfd3c7b7f5257f2f43ff1d5a649be30a1
Thaislaa <thaisladaveiga@gmail.com>
2026-05-20 08:12:40 -0300

    importa dotenv/config no prisma

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "../../generated/prisma/client.js"
+import "dotenv/config"
 
 const connectionString = `${process.env.DATABASE_URL}`
 
