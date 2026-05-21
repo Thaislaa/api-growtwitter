eed7103bf6d25deccb691807142b04707a71f670
Thaislaa <thaisladaveiga@gmail.com>
2026-05-21 10:14:03 -0300

    adiciona tweetRoutes no app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { userRoutes } from "./routes/user.routes.js";
+import { tweetRoutes } from "./routes/tweet.routes.js";
 
 const app = express();
 
 app.use(express.json())
 
 app.use(userRoutes)
+app.use(tweetRoutes)
 
 export { app }
