d94dfb2dd31aba2bff2ea486c0034fdfa0fc4865
Thaislaa <thaisladaveiga@gmail.com>
2026-05-27 09:53:05 -0300

    adiciona autenticação de usuário

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,49 @@
+import type { Request, Response } from "express";
+import { UserService } from "../services/user.services.js"
+import { handleError } from "../utils/handle.error.js"
+
+export class AuthController {
+    private userService = new UserService();
+
+    public async login(req: Request, res: Response) {
+        try {
+            const { username, senha } = req.body
+
+            if (!username || !senha) {
+                return res.status(400).send({
+                    ok: false,
+                    message: "Informe todos os campos."
+                })
+            }
+
+            const user = await this.userService.getByUsername(username)
+
+            if (!user) {
+                return res.status(401).send({
+                    ok: false,
+                    message: "Usuário não encontrado."
+                })
+            }
+
+            if (user.senha !== senha) {
+                return res.status(401).send({
+                    ok: false,
+                    message: "Senha incorreta."
+                })
+            }
+
+            return res.status(200).send({
+                ok: true,
+                message: "Login realizado com sucesso!",
+                data: {
+                    id: user.id,
+                    username: user.username,
+                    nome: user.nome,
+                    imagemUrl: user.imagemUrl
+                }
+            })
+        } catch (error) {
+            return handleError(error, res)
+        }
+    }
+}
